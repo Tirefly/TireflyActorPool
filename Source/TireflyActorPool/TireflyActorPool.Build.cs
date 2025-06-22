fa29fabfbbde514e2635d20283a07dc4b77409be
Tirefly <tzlUltraGames@outlook.com>
2025-06-22 17:43:21 +0800

    # delete 移除Struct依赖

--- Source/TireflyActorPool/TireflyActorPool.Build.cs
+++ Source/TireflyActorPool/TireflyActorPool.Build.cs
@@ -41,7 +41,6 @@ public class TireflyActorPool : ModuleRules
 				"SlateCore",
 				"AIModule",
                 "Niagara",
-                "StructUtils"
 				// ... add private dependencies that you statically link with here ...	
 			}
 			);
